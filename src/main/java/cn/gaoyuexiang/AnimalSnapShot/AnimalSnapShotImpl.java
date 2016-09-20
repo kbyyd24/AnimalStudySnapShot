bb312048a5b6037bc153e21d4726f7ef7b145e29
gaoyx <melo@gaoyuexiang.cn>
2016-09-20 14:48:17 +0800

    [#6] finish sort

--- src/main/java/cn/gaoyuexiang/AnimalSnapShot/AnimalSnapShotImpl.java
+++ src/main/java/cn/gaoyuexiang/AnimalSnapShot/AnimalSnapShotImpl.java
@@ -1,8 +1,19 @@
 package cn.gaoyuexiang.AnimalSnapShot;
 
+import cn.gaoyuexiang.AnimalSnapShot.model.AnimalSnapShot;
+import cn.gaoyuexiang.AnimalSnapShot.model.RealSnapShot;
+import cn.gaoyuexiang.AnimalSnapShot.model.SnapShot;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class AnimalSnapShotImpl implements SnapShotable {
 	@Override
 	public String getSnapShot(String historyData, String id) {
+		List<SnapShot> maps = SnapShot.buildList(historyData);
+		maps.sort((map1, map2) ->	(int) (map1.getTime() - map2.getTime()));
+		// TODO: 16-9-20 reduce map to RealSnapShot list
+
 		return null;
 	}
 }
