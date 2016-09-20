faec98f18102b6a8b3f228df00d9b7ec76963423
gaoyx <melo@gaoyuexiang.cn>
2016-09-20 10:36:16 +0800

    [#2] 添加数据冲突异常的测试

--- src/test/java/cn/gaoyuexiang/AnimalSnapShot/AnimalSnapShotImplTest.java
+++ src/test/java/cn/gaoyuexiang/AnimalSnapShot/AnimalSnapShotImplTest.java
@@ -1,5 +1,6 @@
 package cn.gaoyuexiang.AnimalSnapShot;
 
+import cn.gaoyuexiang.AnimalSnapShot.exception.ConflictDataException;
 import cn.gaoyuexiang.AnimalSnapShot.exception.InvalidDataFormatException;
 import org.junit.Test;
 
@@ -30,7 +31,7 @@ public class AnimalSnapShotImplTest {
 	}
 
 	@Test(expected = InvalidDataFormatException.class)
-	public void should_throw_InvalidFormatException() throws Exception {
+	public void should_throw_InvalidDataFormatException() throws Exception {
 		String historyData =
 						"e4e87cb2-8e9a-4749-abb6-26c59344dfee\n" +
 										"2016/09/02 22:30:46\n" +
@@ -49,5 +50,29 @@ public class AnimalSnapShotImplTest {
 		AnimalSnapShotImpl animalMap = new AnimalSnapShotImpl();
 		String actual = animalMap.getSnapShot(historyData, id);
 		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void should_throw_ConflictDataException() throws Exception {
+		String historyData =
+						"e4e87cb2-8e9a-4749-abb6-26c59344dfee\n" +
+										"2016/09/02 22:30:46\n" +
+										"cat1 10 9\n\n" +
+										"351055db-33e6-4f9b-bfe1-16f1ac446ac1\n" +
+										"2016/09/02 22:30:52\n" +
+										"cat1 10 9 2 -1\n" +
+										"cat2 2 3\n\n" +
+										"dcfa0c7a-5855-4ed2-bc8c-4accae8bd155\n" +
+										"2016/09/02 22:31:02\n" +
+										"cat1 12 10 3 4";
+		String id = "dcfa0c7a-5855-4ed2-bc8c-4accae8bd155";
+		String expected = "Conflict found at " + id;
+		AnimalSnapShotImpl animalMap = new AnimalSnapShotImpl();
+		try {
+			animalMap.getSnapShot(historyData, id);
+		} catch (ConflictDataException cdException) {
+			String message = cdException.getMessage();
+			assertEquals(expected, message);
+		}
 	}
 }
