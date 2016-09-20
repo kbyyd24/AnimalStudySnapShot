928f56b7700151c6587d9608357eb6c1bed6e01c
gaoyx <melo@gaoyuexiang.cn>
2016-09-20 10:28:05 +0800

    [#5] 创建InvalidDataFormatException

---
+++ src/test/java/cn/gaoyuexiang/AnimalSnapShot/exception/InvalidDataFormatException.java
@@ -0,0 +1,26 @@
+package cn.gaoyuexiang.AnimalSnapShot.exception;
+
+public class InvalidDataFormatException extends RuntimeException {
+
+	public InvalidDataFormatException() {
+		super();
+	}
+
+	public InvalidDataFormatException(String message) {
+		super(message);
+	}
+
+	public InvalidDataFormatException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public InvalidDataFormatException(Throwable cause) {
+		super(cause);
+	}
+
+	protected InvalidDataFormatException(String message, Throwable cause,
+	                           boolean enableSuppression,
+	                           boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+}
