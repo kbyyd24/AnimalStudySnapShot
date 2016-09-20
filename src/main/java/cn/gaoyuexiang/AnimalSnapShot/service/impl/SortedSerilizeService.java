0a0069491d1ca79ddc98cb01beac4954b91f9a89
gaoyx <melo@gaoyuexiang.cn>
2016-09-20 22:43:16 +0800

    [#8] add SortedSerilizeService

---
+++ src/main/java/cn/gaoyuexiang/AnimalSnapShot/service/impl/SortedSerilizeService.java
@@ -0,0 +1,13 @@
+package cn.gaoyuexiang.AnimalSnapShot.service.impl;
+
+import cn.gaoyuexiang.AnimalSnapShot.model.SnapShot;
+import cn.gaoyuexiang.AnimalSnapShot.service.SerilizeService;
+
+import java.util.List;
+
+public class SortedSerilizeService implements SerilizeService {
+	@Override
+	public List<SnapShot> parseToList(String historyData) {
+		return null;
+	}
+}
