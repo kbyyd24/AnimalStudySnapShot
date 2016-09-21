a0b2b7e04592c6144fac69f224b508aa8aa9951e
gaoyx <melo@gaoyuexiang.cn>
2016-09-21 18:56:36 +0800

    [#11] 重构AnimalSnapShotImpl，调用服务实现功能

--- src/main/java/cn/gaoyuexiang/AnimalSnapShot/AnimalSnapShotImpl.java
+++ src/main/java/cn/gaoyuexiang/AnimalSnapShot/AnimalSnapShotImpl.java
@@ -1,94 +1,46 @@
 package cn.gaoyuexiang.AnimalSnapShot;
 
-import cn.gaoyuexiang.AnimalSnapShot.exception.ConflictDataException;
-import cn.gaoyuexiang.AnimalSnapShot.exception.InvalidDataFormatException;
-import cn.gaoyuexiang.AnimalSnapShot.model.AnimalSnapShot;
-import cn.gaoyuexiang.AnimalSnapShot.model.RealAnimalSnapShot;
 import cn.gaoyuexiang.AnimalSnapShot.model.RealSnapShot;
 import cn.gaoyuexiang.AnimalSnapShot.model.SnapShot;
+import cn.gaoyuexiang.AnimalSnapShot.service.MapService;
+import cn.gaoyuexiang.AnimalSnapShot.service.SearchService;
+import cn.gaoyuexiang.AnimalSnapShot.service.SerializeService;
+import cn.gaoyuexiang.AnimalSnapShot.service.impl.RealSnapShotMapService;
+import cn.gaoyuexiang.AnimalSnapShot.service.impl.SearchSnapShotService;
+import cn.gaoyuexiang.AnimalSnapShot.service.impl.SortedSerializeService;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 public class AnimalSnapShotImpl implements SnapShotable {
+
+	private SerializeService serializeService;
+	private MapService mapService;
+	private SearchService searchService;
+	private List<RealSnapShot> realSnapShots;
+
+	public AnimalSnapShotImpl(SerializeService serializeService,
+	                          MapService mapService,
+	                          SearchService searchService) {
+		this.serializeService = serializeService;
+		this.mapService = mapService;
+		this.searchService = searchService;
+	}
+
+	public AnimalSnapShotImpl() {
+		this.serializeService = new SortedSerializeService();
+		this.mapService = new RealSnapShotMapService();
+		this.searchService = new SearchSnapShotService();
+	}
+
 	@Override
 	public String getSnapShot(String historyData, String id) {
 		try {
-			//split historyData and save into a list
-			List<SnapShot> maps = SnapShot.buildList(historyData);
-			//sort the list by time
-			maps.sort((map1, map2) -> (int) (map1.getTime() - map2.getTime()));
-			//map the list to be a list of RealSnapShot
-			List<RealSnapShot> snapShots = new ArrayList<>(maps.size());
-			mapAnimalsList(maps, snapShots);
-			//search snapshot by id and return result
-			RealSnapShot realSnapShot = snapShots.stream()
-							.filter(snapShot -> snapShot.getId().equals(id))
-							.findFirst()
-							.orElse(new RealSnapShot());
-			if (realSnapShot.getId() == null) return "";
-			realSnapShot.getAnimals()
-							.sort((a1, a2) ->
-											a1.getName().compareTo(a2.getName()));
-			return realSnapShot.getResult();
-		} catch (ConflictDataException cdException) {
-			return cdException.getMessage();
-		} catch (InvalidDataFormatException infException) {
-			return "Invalid format";
+			List<SnapShot> snapShots = serializeService.parseToList(historyData);
+			realSnapShots = mapService.map(snapShots);
+			RealSnapShot aimSnapShot = searchService.search(realSnapShots, id);
+			return aimSnapShot.getResult();
+		} catch (Exception e) {
+			return e.getMessage();
 		}
-	}
-
-	private void mapAnimalsList(List<SnapShot> maps, List<RealSnapShot> snapShots) {
-		maps.forEach(map -> {
-			RealSnapShot snapShot = new RealSnapShot(map.getId(), map.getTime());
-			List<AnimalSnapShot> animals = map.getAnimals();
-			if (snapShots.size() > 0) {
-				List<RealAnimalSnapShot> preAnimals =
-								snapShots.get(snapShots.size() - 1).getAnimals();
-				Predicate<AnimalSnapShot> predicate = animal ->
-								animal.getMovingX() == 0 && animal.getMovingY() == 0;
-				Map<Boolean, List<AnimalSnapShot>> classifiedLists = animals
-								.stream()
-								.collect(Collectors.partitioningBy(predicate));
-				List<AnimalSnapShot> newAnimals = classifiedLists.get(true);
-				List<AnimalSnapShot> existedAnimals = classifiedLists.get(false);
-				snapShot.addAnimalsByAnimalSnapShots(newAnimals);
-				mapExistedAnimals(snapShots, map, snapShot, preAnimals, existedAnimals);
-			} else {
-				setFirstSnapShot(snapShots, snapShot, animals);
-			}
-		});
-	}
-
-	private void mapExistedAnimals(List<RealSnapShot> snapShots,
-	                               SnapShot map,
-	                               RealSnapShot snapShot,
-	                               List<RealAnimalSnapShot> preAnimals,
-	                               List<AnimalSnapShot> existedAnimals) {
-		preAnimals.forEach(animal -> {
-			String name = animal.getName();
-			AnimalSnapShot movingAnimal = existedAnimals.stream()
-							.filter(item -> item.getName().equals(name))
-							.findFirst()
-							.orElse(new AnimalSnapShot());
-			if (movingAnimal.getName() != null) {
-				if (animal.isConflicted(movingAnimal))
-					throw new ConflictDataException("Conflict found at " + map.getId());
-				snapShot.addAnimal(new RealAnimalSnapShot(animal.getName(),
-								movingAnimal.getPreviousX() + movingAnimal.getMovingX(),
-								movingAnimal.getPreviousY() + movingAnimal.getMovingY()));
-			} else {
-				snapShot.addAnimal(animal);
-			}
-				snapShots.add(snapShot);
-		});
-	}
-
-	private void setFirstSnapShot(List<RealSnapShot> snapShots, RealSnapShot snapShot, List<AnimalSnapShot> animals) {
-		snapShot.addAnimalsByAnimalSnapShots(animals);
-		snapShots.add(snapShot);
 	}
 }
