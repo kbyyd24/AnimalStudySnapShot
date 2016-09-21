3578c1c0c5a6a27e5bbfbc4af045877f79cecb6f
gaoyx <melo@gaoyuexiang.cn>
2016-09-21 15:50:34 +0800

    [#9] 添加转换数据的实现类RealSnapShotMapService

---
+++ src/main/java/cn/gaoyuexiang/AnimalSnapShot/service/impl/RealSnapShotMapService.java
@@ -0,0 +1,14 @@
+package cn.gaoyuexiang.AnimalSnapShot.service.impl;
+
+import cn.gaoyuexiang.AnimalSnapShot.model.RealSnapShot;
+import cn.gaoyuexiang.AnimalSnapShot.model.SnapShot;
+import cn.gaoyuexiang.AnimalSnapShot.service.MapService;
+
+import java.util.List;
+
+public class RealSnapShotMapService implements MapService {
+	@Override
+	public List<RealSnapShot> map(List<SnapShot> snapShots) {
+		return null;
+	}
+}
