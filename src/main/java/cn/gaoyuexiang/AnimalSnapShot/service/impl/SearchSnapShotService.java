9d76cf831b18d8ed5dc739d3da49d7a61ef2b3ab
gaoyx <melo@gaoyuexiang.cn>
2016-09-21 18:32:22 +0800

    [#10] 查找服务通过测试

--- src/main/java/cn/gaoyuexiang/AnimalSnapShot/service/impl/SearchSnapShotService.java
+++ src/main/java/cn/gaoyuexiang/AnimalSnapShot/service/impl/SearchSnapShotService.java
@@ -1,12 +1,23 @@
 package cn.gaoyuexiang.AnimalSnapShot.service.impl;
 
+import cn.gaoyuexiang.AnimalSnapShot.exception.NoSuchSnapShotException;
+import cn.gaoyuexiang.AnimalSnapShot.model.RealSnapShot;
 import cn.gaoyuexiang.AnimalSnapShot.service.SearchService;
 
 import java.util.List;
+import java.util.Optional;
 
 public class SearchSnapShotService implements SearchService {
+
 	@Override
-	public <T> T search(List<T> snapShots, String id) {
-		return null;
+	public RealSnapShot search(List<RealSnapShot> snapShots, String id) {
+		Optional<RealSnapShot> matchSnapShot = snapShots
+						.stream()
+						.filter(item -> item.getId().equals(id))
+						.findFirst();
+		if (matchSnapShot.isPresent()) {
+			return matchSnapShot.get();
+		}
+		throw new NoSuchSnapShotException("Not found " + id);
 	}
 }
