3673e21e3cbb75a65331fbdb02f87010a9a3610c
gaoyx <melo@gaoyuexiang.cn>
2016-09-21 17:56:30 +0800

    [#10] 添加查找服务实现类SearchSnapShotService

---
+++ src/main/java/cn/gaoyuexiang/AnimalSnapShot/service/impl/SearchSnapShotService.java
@@ -0,0 +1,12 @@
+package cn.gaoyuexiang.AnimalSnapShot.service.impl;
+
+import cn.gaoyuexiang.AnimalSnapShot.service.SearchService;
+
+import java.util.List;
+
+public class SearchSnapShotService implements SearchService {
+	@Override
+	public <T> T search(List<T> snapShots, String id) {
+		return null;
+	}
+}
