fd79fe5b4e24df38e3a49f77effdcc30b6c382f1
gaoyx <melo@gaoyuexiang.cn>
2016-09-22 12:02:39 +0800

    [#14] update AnimalSanpShotImpl

--- src/main/java/cn/gaoyuexiang/AnimalSnapShot/AnimalSnapShotImpl.java
+++ src/main/java/cn/gaoyuexiang/AnimalSnapShot/AnimalSnapShotImpl.java
@@ -10,13 +10,15 @@ import cn.gaoyuexiang.AnimalSnapShot.service.impl.SearchSnapShotService;
 import cn.gaoyuexiang.AnimalSnapShot.service.impl.SortedSerializeService;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class AnimalSnapShotImpl implements SnapShotable {
 
 	private SerializeService serializeService;
 	private MapService mapService;
 	private SearchService searchService;
-	private List<RealSnapShot> realSnapShots;
+	private Map<String, RealSnapShot> realSnapShots;
 
 	public AnimalSnapShotImpl(SerializeService serializeService,
 	                          MapService mapService,
@@ -36,7 +38,9 @@ public class AnimalSnapShotImpl implements SnapShotable {
 	public String getSnapShot(String historyData, String id) {
 		try {
 			List<SnapShot> snapShots = serializeService.parseToList(historyData);
-			realSnapShots = mapService.map(snapShots);
+			realSnapShots = mapService.map(snapShots)
+							.stream()
+							.collect(Collectors.toMap(RealSnapShot::getId, snapShot -> snapShot));
 			RealSnapShot aimSnapShot = searchService.search(realSnapShots, id);
 			return aimSnapShot.getResult();
 		} catch (Exception e) {
